7021d1d1f4a5c30e1c33a1c00c1d27f1d68988ef
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-10 11:04:12 +0300

    js event for form to handle inputs and create new object when user add from form

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+
+// Add event listener to the form submission
+document.querySelector('#animal-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get the input values
+  const nameInput = document.querySelector('#nameInput').value;
+  const imageInput = document.querySelector('#imageInput').value;
+  const descriptionInput = document.querySelector('#descriptionInput').value;
+
+  // Create a new animal object
+  const newAnimal = {
+    nameInput: nameInput,
+    imageInput: imageInput,
+    descriptionInput: descriptionInput,
+    donations: 0
+  };
+
+  // Call the handlePostRequest function to send the POST request
+  handlePostRequest(newAnimal);
+});
+
